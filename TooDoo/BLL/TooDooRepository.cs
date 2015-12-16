f8374d562f6ca1a8311e02dd02219883bff5fe4b
Johanna Funkquist <johanna@tomta.se>
2015-12-16 23:41:40 +0100

    grundprojekt

---
+++ TooDoo/BLL/TooDooRepository.cs
@@ -0,0 +1,19 @@
+﻿using Core.Model;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class TooDooRepository
+    {
+        public void AddToDo(ToDo toDo)
+        {
+            var dataRepository = new DataRepository();
+            dataRepository.AddToDo(toDo);
+        }
+    }
+}
---
+++ TooDoo/DAL/DataRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DataRepository : DAL
+    {
+        private const string _connectionString = @"Data Source = server name; Initial Catalog = DB_ToDoList; User ID = RestFullUser; Password = RestFull123";
+        
+        public DataRepository() : base (_connectionString)
+        {
+
+        }    
+    }
+
+
+}
