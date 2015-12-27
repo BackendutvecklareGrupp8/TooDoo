f220bb5efa537e95dd8ce7f16f7cf60680474576
Johanna Funkquist <johanna@tomta.se>
2015-12-27 23:51:29 +0100

    Har lyft ut metoder till egna klasser i konsol-projektet. Menyval 1 i första vyn fungerar.

--- TooDoo/TooDooUI/List.cs
+++ TooDoo/TooDooUI/List.cs
@@ -3,12 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ToDoList;
 
 namespace TooDooUI
 {
-    public class ToDoListsWrapper
+    class List
     {
-        public List<ToDoList> d { get; set; }
     }
 }
--- TooDoo/TooDooUI/Program.cs
+++ TooDoo/TooDooUI/Program.cs
@@ -13,8 +13,16 @@ namespace ToDoList
     {
         static void Main(string[] args)
         {
-            //PrintToDoLists();
+            Navigation.GoToStart();
+        }   
 
+        
+
+
+
+
+        private static void PrintToDoList()
+        {
             //Metod för att göra ett API-anrop via REST, få tillbaka en JSON-sträng, deserialisera (omforma) den till ett objekt 
 
             var url = "http://localhost:55020/ToDoListService.svc/GetToDoListByName";   // Min lokala endpoint till API:et och metoden GetToDoListByName
@@ -44,41 +52,8 @@ namespace ToDoList
                 Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);    // Felhantering
             }
 
-
         }
 
-        private static void PrintToDoLists()
-        {
-            //Metod för att göra ett API-anrop via REST, få tillbaka en JSON-sträng, deserialisera (omforma) den till ett objekt 
-
-            var url = "http://localhost:55020/ToDoListService.svc/GetToDoLists";    // Min lokala endpoint till API:et och metoden GetToDoListByName
-            var urlParameters = "";                                                 // URL-parametern, i det här fallet namnet på listan
-
-            HttpClient client = new HttpClient();   //Klass som sköter kommunikationen över nätverket/internet
-            client.BaseAddress = new Uri(url);      //Sätter url:en på httpclient-objektet
-
-            Console.WriteLine("Tryck enter för att starta anropet");
-            Console.ReadLine();                                         //Stopp för att säkerställa att API hinner starta innan konsolen efterfrågar något
-
-            client.DefaultRequestHeaders.Accept.Add(                    //Sätter en header i anropet för att tala om vilken typ av data den vill ha tillbaka
-            new MediaTypeWithQualityHeaderValue("application/json"));
-
-            HttpResponseMessage response = client.GetAsync(urlParameters).Result;   // Här görs http-requesten. Metoden GetAsync returnerar ett response-objekt
-            if (response.IsSuccessStatusCode)                                       // Kontrollerar om http-status är 200-nånting (success)
-            {
-                var toDoListWrapper = response.Content.ReadAsAsync<ToDoListsWrapper>().Result;   // Gör om JSON-strängen till ett objekt av typen ToDoListWrapper
-
-                foreach (var d in toDoListWrapper.d)                                            // För varje ToDo i ToDoListWrapper
-                {
-                    Console.WriteLine("{0}", d.Name);                                           // Skriv ut radens namn    
-                }
-            }
-            else
-            {
-                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);    // Felhantering
-            }
-
-
-        }
+        
     }
 }
